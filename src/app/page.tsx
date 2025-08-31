bd6c778dff3e92ac8aa75cad45238cbf7067faaa
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-08-31 13:35:55 +0000

    Elimina el header y el logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { AnimatedHero } from '@/components/animated-hero';
-import { Header } from '@/components/header';
 
 export default function Home() {
   return (
     <div className="bg-background text-foreground">
-      <Header />
       <main>
         <section className="flex flex-col items-center justify-center h-screen px-4">
           <div className="max-w-4xl w-full h-full flex">

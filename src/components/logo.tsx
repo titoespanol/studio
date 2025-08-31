97930be4dbecb55e5ecbc3378de34b4f6298fdab
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-08-31 13:40:24 +0000

    No se abre correctamente. Te acabo de pasar el logo en -gif a ver si así

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="/logo.png"
+      src="/logo.gif"
       alt="Child Lens Logo"
       width={200}
       height={45}
       className="w-full h-auto dark:filter dark:invert"
       priority
+      unoptimized
     />
   );
 }

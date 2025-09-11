257c22f590a91679900580a4cdae719f9df2439d
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-09-11 07:56:30 +0000

    Un poco más grande el logo del header de The Child lens?

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,9 @@
+
 import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="relative w-32 h-10">
+    <div className="relative w-40 h-12">
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/child-lens-landing.firebasestorage.app/o/TimesBlack.png?alt=media&token=77cbe246-5974-4dfd-ab91-2d4f830f91e2"
         alt="The Child Lens Logo"

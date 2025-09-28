7f88177dada38a178040be65541cb9e65d8f9abd
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-09-28 11:49:25 +0000

    cambia el icono de linkedin para que sea más fino y elegante

--- src/components/who-we-are.tsx
+++ src/components/who-we-are.tsx
@@ -34,7 +34,7 @@ const ElegantLinkedinIcon = () => (
         viewBox="0 0 24 24" 
         fill="none" 
         stroke="currentColor" 
-        strokeWidth="2" 
+        strokeWidth="1" 
         strokeLinecap="round" 
         strokeLinejoin="round" 
         className="w-5 h-5"

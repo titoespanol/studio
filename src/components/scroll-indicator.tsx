016d747c4a88b07f1fd957b8eb7de8daa5a0f1f1
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-08 20:00:13 +0000

    Quita el texto de scroll. Y que la animación sólo la haga 3 veces

--- src/components/scroll-indicator.tsx
+++ src/components/scroll-indicator.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-
 export function ScrollIndicator() {
   return (
     <div className="flex flex-col items-center">
       <div className="mouse" />
-      <p className={cn("scroll-indicator-text")}>
-        SCROLL
-      </p>
     </div>
   );
 }

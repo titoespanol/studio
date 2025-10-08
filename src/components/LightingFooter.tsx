21504a5ce18cefe1f6ae968692224c9812531c21
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-08 20:15:05 +0000

    No veo los colores del footer. lo puedes hacer más visible?

---
+++ src/components/LightingFooter.tsx
@@ -0,0 +1,19 @@
+
+export function LightingFooter() {
+    return (
+      <footer id="lighting-wrap">
+        <div id="lightings">
+          <section id="one">
+            <section id="two">
+              <section id="three">
+                <section id="four">
+                  <section id="five" />
+                </section>
+              </section>
+            </section>
+          </section>
+        </div>
+      </footer>
+    );
+  }
+  

7a8a65ce25d3879edb9ce4e02c8cc1b6476b6cfe
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-12 07:02:23 +0000

    yo ya he cambiado la url en src/components/logo.tsx por la nueva

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <div className="relative w-40 h-12 flex items-center justify-center">
       <Image
-        src="https://firebasestorage.googleapis.com/v0/b/child-lens-landing.firebasestorage.app/o/The%20Child%20Lens%20Logo.png?alt=media&token=0eb31b19-03b9-4539-a922-5700a17281dd"
+        src="https://firebasestorage.googleapis.com/v0/b/child-lens-landing.firebasestorage.app/o/The%20Child%20Lens%20Logo.png?alt=media&token=6ea0a8fe-c02b-4aeb-8452-3e6a45083ca7"
         alt="The Child Lens Logo"
         width={160}
         height={48}

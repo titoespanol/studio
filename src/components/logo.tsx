89d970b249985a47cdcbb7f906ef4c876737b8ce
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-12 17:22:06 +0000

    replace logo with local /logotcl.png version

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,11 @@
-
-import Image from 'next/image';
-
 export function Logo() {
   return (
     <div className="relative w-40 h-12 flex items-center justify-center">
-      <Image
+      <img
         src="/logotcl.png"
-        alt="The Child Lens Logo"
-        width={160}
-        height={48}
-        priority
+        alt="The Child Lens logo"
+        width="160"
+        height="48"
       />
     </div>
   );

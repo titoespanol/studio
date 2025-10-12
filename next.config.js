1bb3cdcb183863baeb22449b43761296155719aa
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-12 18:55:24 +0000

    sigo viendo esto. Puedes mostrarme qué url estás usando tu?

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
-        pathname: '/v0/b/child-lens-landing.firebasestorage.app/o/**',
+        pathname: '/**',
       },
       {
         protocol: 'https',

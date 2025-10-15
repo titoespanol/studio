00e3fe93f11ee1cd006059e5951bb363a95f2b5d
Pilar Puig-Sarries <pilarpuig@gmail.com>
2025-10-15 06:21:09 +0000

    Continua el problema: Se produjo un error en la compilación error in cre

--- next.config.js
+++ next.config.js
@@ -1,40 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    loader: 'custom',
-    loaderFile: './src/lib/image-loader.js',
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestudio.app',
-        port: '',
-        pathname: '/**',
-      }
-    ],
   },
 };
 
